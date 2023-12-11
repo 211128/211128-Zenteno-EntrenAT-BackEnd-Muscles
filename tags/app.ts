20b531df06f6ee4fec72f0004f179b40b71e3518
211128 <84791442+211128@users.noreply.github.com>
2023-12-11 05:06:21 -0600

    Add files via upload

---
+++ tags/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { Signale } from 'signale';
+import { armRouter } from './src/honortags/infraestructure/armRoutes';
+
+const app = express();
+const signale = new Signale();
+
+app.use(express.json());
+
+// Rutas relacionadas con usuarios
+app.use(armRouter)
+
+
+app.listen(8080, () => {
+    signale.success("Server online in port 8080");
+});
+
